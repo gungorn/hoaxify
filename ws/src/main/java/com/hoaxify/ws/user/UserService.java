779499964f99c1535f17780cdf53a618ed4984cf
gungorn <gungorn@outlook.com>
2021-01-20 16:03:59 +0300

    password => password crypt

--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,16 +16,23 @@ public class UserService {
 
 	UserRepository userRepository;
 	
+	
+	PasswordEncoder passwordEncoder;
+	
 	@Autowired
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	////////////////////////////////////////////////////////////////
 
 
 
 	public void save(User user) {
+user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+				
 		userRepository.save(user);
 	}
 }
