4e8fdf1c8c75e0f1b067f80f16c121fa52cf8d3d
gungorn <gungorn@outlook.com>
2021-01-20 12:01:56 +0300

    lombok data annotation usage

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,21 +1,24 @@
 package com.hoaxify.ws.user;
 
+import lombok.Data;
+
+@Data //getter/setter - tostring - constructor - vs vs
 public class User { //user model
 	private String username;
 	private String displayname;
 	private String password;
 
-	public String getUsername() { return username; }
-	public void setUsername(String username) { this.username = username; }
+	// public String getUsername() { return username; }
+	// public void setUsername(String username) { this.username = username; }
 
-	public String getDisplayname() { return displayname; }
-	public void setDisplayname(String displayname) { this.displayname = displayname; }
+	// public String getDisplayname() { return displayname; }
+	// public void setDisplayname(String displayname) { this.displayname = displayname; }
 
-	public String getPassword() { return password; }
-	public void setPassword(String password) { this.password = password; }
+	// public String getPassword() { return password; }
+	// public void setPassword(String password) { this.password = password; }
 
-	@Override
-	public String toString() {
-		return "User [username=" + username + ", displayname=" + displayname + ", password=" + password + "]";
-	}
+	// @Override
+	// public String toString() {
+	// 	return "User [username=" + username + ", displayname=" + displayname + ", password=" + password + "]";
+	// }
 }
