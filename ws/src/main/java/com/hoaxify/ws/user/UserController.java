4164265c93f605a2e56308d70f0beb503db2f100
gungorn <gungorn@outlook.com>
2021-01-26 13:22:41 +0300

    ResponseEntity

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,15 +1,21 @@
 package com.hoaxify.ws.user;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
@@ -20,13 +26,31 @@ public class UserController {
 	UserService userService;
 
 	//@CrossOrigin //farklı portlardan erişim
+	//@ResponseStatus(HttpStatus.CREATED) //request status değiştirme (default : 200 OK)
 	@PostMapping("/api/v1/users") //post url
-	@ResponseStatus(HttpStatus.CREATED) //request status değiştirme (default : 200 OK)
-	public GenericResponse createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@RequestBody User user) {
 		log.info("1> " + user.toString());
 
+		String username = user.getUsername();
+		String password = user.getPassword();
+
+		//if(username == null || username.isEmpty()) return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+
+
+		//username validation error
+		if(username == null || username.isEmpty()) {
+			ApiError error = new ApiError(400, "Validation Error", "api/v1/users"); //bu request'teki hatalar
+
+			Map<String, String> validationErrors = new HashMap<>();
+			validationErrors.put("username", "Username cannot be null");
+			error.setValidationErrors(validationErrors);
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
+
+
 		userService.saveUser(user); //service'e bağlan
-		
-		return new GenericResponse("dandini dandini dastana"); //http request response
+
+		return ResponseEntity.ok(new GenericResponse("her şey yolunda"));
 	}
 }
